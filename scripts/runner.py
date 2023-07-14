254b1baed66c28189634f28f3be2273fa83dd354
Christoffer Jepsen <Christoffer.ssj@gmail.com>
2023-07-14 11:58:51 +0200

    Made the runner get the scripts and create objects

--- scripts/runner.py
+++ scripts/runner.py
@@ -1,13 +1,13 @@
 import os
+import re
 
-class Runner:
-    def __init__(self):
-        self.script = get_scipts()
+class Script:
+    def __init__(self, scriptname, author, docstring='', options=[]):
+        self.scriptname = scriptname
+        self.docstring = docstring
+        self.author = author
+        self.options = options
 
-def get_scipts():
-    scripts = []
-    for f in os.listdir():
-        if '__' not in f and 'runner.py' not in f:
-            scripts.append(f)
-    return scripts
-
+def get_scripts():
+    scripts = [f for f in os.listdir('scripts/') if '__' not in f and 'runner.py' not in f]
+    return{script: Script(scriptname=script, docstring=re.findall('(?s)"""(.*?)"""', open(f'scripts/{script}', 'r').read())) for script in scripts}
